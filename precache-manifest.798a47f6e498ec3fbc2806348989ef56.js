1661cc4837518e8c705e870d3baff4985ddf239b
MishaMiron <53948817+MykhailoEvtukh@users.noreply.github.com>
2020-03-16 15:19:17 +0200

    Updates

---
+++ precache-manifest.798a47f6e498ec3fbc2806348989ef56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c968a4b918a30767d5a8d96b9f443c21",
+    "url": "/react-phonebook/index.html"
+  },
+  {
+    "revision": "1e4f0a717113b56478e3",
+    "url": "/react-phonebook/static/css/main.233cb0dd.chunk.css"
+  },
+  {
+    "revision": "07a015392f8bbba6a88c",
+    "url": "/react-phonebook/static/js/2.444325ec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-phonebook/static/js/2.444325ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e4f0a717113b56478e3",
+    "url": "/react-phonebook/static/js/main.f6bf3c8d.chunk.js"
+  },
+  {
+    "revision": "45b7647c82bfc2604580",
+    "url": "/react-phonebook/static/js/runtime-main.98556b6c.js"
+  }
+]);
